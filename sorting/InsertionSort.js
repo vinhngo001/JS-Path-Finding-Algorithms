6716615d68b7f7e37260a7b2d935095dc354e536
Henry Ngo <121779569+vinhngo001@users.noreply.github.com>
2023-07-22 13:44:00 +0700

    asd

--- sorting/InsertionSort.js
+++ sorting/InsertionSort.js
@@ -1,11 +1,10 @@
 function insertionSort(arr) {
-    var key, n = arr.length;
-    for (let i = 0; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
+    for (let i = 0; i < arr.length; i++) {
+        let key = arr[i];
+        let j = i - 1;
         while (j >= 0 && arr[j] > key) {
-            arr[i + 1] = arr[j];
-            j = j - 1;
+            arr[j + 1] = arr[j];
+            j--;
         }
         arr[j + 1] = key;
     }
